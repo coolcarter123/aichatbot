43b3ed350a6ee089399a892373cb52282351055e
coolcarter123 <165233194+coolcarter123@users.noreply.github.com>
2024-05-09 15:50:17 -0400

    Update hehe.py

--- hehe.py
+++ hehe.py
@@ -1,10 +1,18 @@
-import openai
-client = openai.OpenAI(
-base_url = "https://integrate.api.nvidia.com/v1",
-api_key="YOUR_NVIDIA_API_KEY"
+iclient = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "$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"
 )
-chat_completion = client.chat.completions.create(
-model="mistralai/mixtral-8x7b-instruct-v0.1",
-messages=[{"role" : "user" , "content" : "Write me a love song" }],
-temperature=0.7
+
+completion = client.chat.completions.create(
+  model="snowflake/arctic",
+  messages=[{"role":"user","content":""}],
+  temperature=0.5,
+  top_p=1,
+  max_tokens=1024,
+  stream=True
 )
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
